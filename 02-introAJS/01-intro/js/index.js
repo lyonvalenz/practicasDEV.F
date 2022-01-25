6fa275f7aeacba637b19b5ec69cf24d58f0654e6
Leonardo Echegaray <leonvvalenzuela@gmail.com>
2022-01-24 21:33:55 -0600

    Intro a JS

---
+++ 02-introAJS/01-intro/js/index.js
@@ -0,0 +1,15 @@
+//Elementos para mostrar la salida de datos
+//Muestra la salida de consola
+console.log('Hola Mundo, desde un archivo externo')
+console.log(5+10)
+console.log(4==='4')
+
+///Muestra la salida en la página
+document.write('Hola Mundo desde una rchivo externo')
+document.write(5+10)
+document.write(4==='4')
+
+//Muestra la salida de alert
+alert.write('Hola Mundo desde una rchivo externo')
+alert.write(5+10)
+alert.write(4==='4')
